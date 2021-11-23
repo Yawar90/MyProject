5af7e5e52b914f71adc55ba7d064e9d2961644d7
unknown <abbasaskari90@gmail.com>
2021-11-23 15:57:37 +0500

    MyApp

---
+++ src/conts/cars.js
@@ -0,0 +1,41 @@
+   
+const cars = [
+    {
+      id: 1,
+      name: 'Accord',
+      price: '4000',
+      like: true,
+      img: require('../assets/Accord.png'),
+      about:
+        '',
+    },
+  
+    {
+      id: 2,
+      name: 'Civic 2017',
+      price: '4560',
+      like: false,
+      img: require('../assets/Civic2017.jpg'),
+      about:"Good cars are safe, fast, affordable to purchase, economical to operate, reliable, capacious, comfortable, and attractive. No, of course you can't get all of those characteristics in one car; each conflicts with one or more of the others. Safe and fast",
+    },
+    {
+      id: 3,
+      name: 'Lancer',
+      price: '4209',
+      like: false,
+      img: require('../assets/lancer.png'),
+      about:"Good cars are safe, fast, affordable to purchase, economical to operate, reliable, capacious, comfortable, and attractive. No, of course you can't get all of those characteristics in one car; each conflicts with one or more of the others. Safe and fast"
+        
+    },
+  
+    {
+      id: 4,
+      name: 'mercedes',
+      price: '203902',
+      like: true,
+      img: require('../assets/mercedes.png'),
+      about:"Good cars are safe, fast, affordable to purchase, economical to operate, reliable, capacious, comfortable, and attractive. No, of course you can't get all of those characteristics in one car; each conflicts with one or more of the others. Safe and fast"
+    },
+  ];
+  
+  export default cars;
---
+++ src/conts/colors.js
@@ -0,0 +1,8 @@
+const COLORS={
+    white:'#fff',
+    dark:'#000',
+    red:'#F52A2A',
+    light:'#f1f1f1',
+    green:'#00B761'
+};
+export default COLORS
